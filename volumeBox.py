0f9ad026f0bd29c5f305c55f63d5f4da3a867a09
Vincetita <vince.tita@gmail.com>
2018-01-06 15:32:27 +0100

    added volume of box

---
+++ volumeBox.py
@@ -0,0 +1,17 @@
+length= int(input("Enter length: "))
+width= int(input("Enter width: "))
+height= int(input("Enter height: "))
+
+def area(length, width):
+        return length*width
+
+def volume(area,height):
+        print(area*height)
+
+volume(area(length, width),height)
+
+
+
+
+        
+        
